0c2656561deffc5e8d346441202ce1c1b64bb392
boda0077 <abdallahali22007@gmail.com>
2026-03-06 23:11:49 +0200

    another fix destination

--- src/pages/destination.jsx
+++ src/pages/destination.jsx
@@ -78,7 +78,7 @@ function Destination() {
         
         {/* Planet Selector */}
         <div className="flex max-md:justify-center max-sm:grid max-sm:grid-cols-2 gap-15">
-          {["europa","mars","moon","titan"].map((name, i) => (
+          {["moon","mars","europa","titan"].map((name, i) => (
             <span
               key={i}
               onClick={() => setIndex(i)}
